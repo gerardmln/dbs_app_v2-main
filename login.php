410917aabc200225f8496b2f125284da77449b4d
gerardmln <malaluangl@students.nu-lipa.edu.ph>
2025-05-22 11:29:41 +0800

    sds

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
 <?php
 
+
 session_start();
+if (isset($_SESSION['admin_id'])) {
+    header("Location: index.php");
+    exit();}
 require_once('classes/database.php');
 $sweetAlertConfig = "";
 $con = new database();
